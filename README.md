c9914bdbaa17980b455a8af548f60464a0eff98c
hilee <hilee@daumsoft.com>
2020-10-13 16:28:19 +0900

    change main.js and README.md

--- README.md
+++ README.md
@@ -7,5 +7,15 @@ JavaScript를 이용해서 Node.js를 제어해 동적으로 HTML 코드를 생�
 기본 설치방법, node.js runtime 사용법, node.js로 APPcat 제작까지 따라해볼 수 있는 훌륭한 튜토리얼이다.
 
 Node.js Version : v12.19.0
+pm2 Version : 4.5.0
+
+```bash
+# pm2 간단 사용법
+pm2 start main.js --watch
+pm2 monit
+pm2 list
+pm2 log
+pm2 stop main.js
+```
 
 ![nodejs](https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg)
--- main.js
+++ main.js
@@ -61,7 +61,13 @@ var app = http.createServer(function (request, response) {
                         title,
                         list,
                         `<h2>${title}</h2>${description}`,
-                        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+                        `<a href="/create">create</a> 
+                         <a href="/update?id=${title}">update</a>
+                         <form action="delete_process" method="post">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                         </form>
+                         `,
                     );
                     response.writeHead(200);
                     response.end(template);
@@ -76,7 +82,7 @@ var app = http.createServer(function (request, response) {
                 title,
                 list,
                 `
-                <form action="http://localhost:3000/create_process" method="post">
+                <form action="/create_process" method="post">
                     <p>
                         <input type="text" name="title" placeholder="title"></p>
                     <p>
@@ -104,6 +110,64 @@ var app = http.createServer(function (request, response) {
             var description = post.description;
             fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
                 response.writeHead(302, { Location: `/?id=${title}` });
+                response.end();
+            });
+        });
+    } else if (pathname == '/update') {
+        fs.readdir('./data', function (error, filelist) {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                var title = queryData.id;
+                var list = templateList(filelist);
+                var template = templateHTML(
+                    title,
+                    list,
+                    `
+                    <form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <p>
+                            <input type="text" name="title" placeholder="title" value="${title}"></p>
+                        <p>
+                            <textarea name="description" placeholder="description">${description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit">
+                        </p>
+                    </form>
+                    `,
+                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`,
+                );
+                response.writeHead(200);
+                response.end(template);
+            });
+        });
+    } else if (pathname == '/update_process') {
+        var body = '';
+        // post로 전송된 데이터 받고 파싱
+        request.on('data', function (data) {
+            body = body + data;
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, function (error) {
+                fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
+                    response.writeHead(302, { Location: `/?id=${title}` });
+                    response.end();
+                });
+            });
+        });
+    } else if (pathname == '/delete_process') {
+        var body = '';
+        request.on('data', function (data) {
+            body = body + data;
+        });
+        request.on('end', function () {
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, function (error) {
+                response.writeHead(302, { Location: `/` });
                 response.end();
             });
         });
